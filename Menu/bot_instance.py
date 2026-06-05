f896179281a1ae6624f8f804b573bc0e3935e9eb
Andrii <andreborodavka@gmail.com>
2026-06-05 09:24:36 +0200

    changed token

--- Menu/bot_instance.py
+++ Menu/bot_instance.py
@@ -1,4 +1,4 @@
 import telebot
-TOKEN = '8784038384:AAHLScSYECox4cmvBjGH9zl5t1bokSBmIuY' #add your token from BotFather
+TOKEN = 'your token' #add your token from BotFather
 
 bot = telebot.TeleBot(TOKEN)
