613dda40c20e888c37fe6bd9bf5743e99c8a5edc
Andrii <andreborodavka@gmail.com>
2026-06-07 16:32:36 +0200

    initiall comit

--- Menu/bot_instance.py
+++ Menu/bot_instance.py
@@ -1,4 +1,8 @@
 import telebot
-TOKEN = 'your token' #add your token from BotFather
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("BOT_TOKEN")#add your token from BotFather
 
 bot = telebot.TeleBot(TOKEN)
